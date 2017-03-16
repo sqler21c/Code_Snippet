0d1cbd75a8d5b82655d043cfd16608c239c05264
sqler21c <sqler21c@gmail.com>
2017-03-16 11:15:55 +0900

    add

---
+++ Java/createthumnailimage.java
@@ -0,0 +1,28 @@
+private void createThumbnail(String filename, int thumbWidth, int thumbHeight, int quality, String outFilename) throws InterruptedException, FileNotFoundException, IOException {
+    Image image = Toolkit.getDefaultToolkit().getImage(filename);
+    MediaTracker mediaTracker = new MediaTracker(new Container());
+    mediaTracker.addImage(image, 0);
+    mediaTracker.waitForID(0);
+    System.out.println(mediaTracker.isErrorAny());
+    double thumbRatio = (double)thumbWidth / (double)thumbHeight;
+    int imageWidth = image.getWidth(null);
+    int imageHeight = image.getHeight(null);
+    double imageRatio = (double)imageWidth / (double)imageHeight;
+    if (thumbRatio < imageRatio) {
+        thumbHeight = (int)(thumbWidth / imageRatio);
+    } else {
+        thumbWidth = (int)(thumbHeight * imageRatio);
+    }
+    BufferedImage thumbImage = new BufferedImage(thumbWidth, thumbHeight, BufferedImage.TYPE_INT_RGB);
+    Graphics2D graphics2D = thumbImage.createGraphics();
+    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    graphics2D.drawImage(image, 0, 0, thumbWidth, thumbHeight, null);
+    BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outFilename));
+    JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+    JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(thumbImage);
+    quality = Math.max(0, Math.min(quality, 100));
+    param.setQuality((float)quality / 100.0f, false);
+    encoder.setJPEGEncodeParam(param);
+    encoder.encode(thumbImage);
+    out.close();
+}
---
+++ Java/usingPDFiText.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+public class GeneratePDF {
+    public static void main(String[] args) {
+        try {
+            OutputStream file = new FileOutputStream(new File("C:\\Test.pdf"));
+            Document document = new Document();
+            PdfWriter.getInstance(document, file);
+            document.open();
+            document.add(new Paragraph("Hello Kiran"));
+            document.add(new Paragraph(new Date().toString()));
+            document.close();
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
