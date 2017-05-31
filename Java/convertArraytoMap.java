b5fa0461dab49c8776a3ae159372fd1a623b1e39
sqler21c <sqler21c@gmail.com>
2017-05-31 16:17:03 +0900

    modde

--- Java/convertArraytoMap.java
+++ Java/convertArraytoMap.java
@@ -2,7 +2,11 @@ import java.util.Map;
 import org.apache.commons.lang.ArrayUtils;
 public class Main {
     public static void main(String[] args) {
-        String[][] countries = { { "United States", "New York" }, { "United Kingdom", "London" }, { "Netherland", "Amsterdam" }, { "Japan", "Tokyo" }, { "France", "Paris" } };
+        String[][] countries = { { "United States", "New York" },
+                                 { "United Kingdom", "London" },
+                                 { "Netherland", "Amsterdam" },
+                                 { "Japan", "Tokyo" },
+                                 { "France", "Paris" } };
         Map countryCapitals = ArrayUtils.toMap(countries);
         System.out.println("Capital of Japan is " + countryCapitals.get("Japan"));
     }
